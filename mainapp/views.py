0b36e16cf4fd9861d0e17b0482ab36dc4ac50cc8
Marián Horňák <marian.sysel.hornak@gmail.com>
2023-11-11 00:07:55 +0200

    Dummy leaderboard

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def leaderboard(request):
-    return render(request, "huhahei/board.html")
+    dummy_scores = [
+        {'name': 'Syseľ', 'score': 1234},
+        {'name': 'Sára', 'score': 1233},
+        {'name': 'Tomáš', 'score': 47},
+        {'name': 'Zajo', 'score': 42},
+        {'name': 'Pankrác', 'score': 3},
+        {'name': 'Servác', 'score': 2},
+        {'name': 'Bonifác', 'score': 1},
+    ]
+    return render(request, "huhahei/board.html", {'records': dummy_scores})
